55639e383235accb8004af039a0c3141d42c13c4
alx <agarkov.aleksei1@yandex.ru>
2026-07-19 19:06:19 +0300

    Update preprocessing.py

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn.preprocessing import FunctionTransformer, MultiLabelBinarizer
 
 
@@ -15,3 +16,40 @@ def col_splitter(col, sep='/'):
 
 def binarizer():
     return FunctionTransformer(MultiLabelBinarizer().fit_transform)
+
+
+def construct_basin_location_mapper(df):
+    basin_location_info = df[['basin_name', 'onshore', 'offshore', 'onshore-offshore-calc']].to_dict("records")
+    basin_location_mapper = {}
+
+    for info in basin_location_info:
+        name = info['basin_name']
+        if info['onshore-offshore-calc'] > 0:
+            location = 'onshore-offshore'
+        elif info['onshore'] > 0 and info['offshore'] == 0:
+            location = 'onshore'
+        elif info['onshore'] == 0 and info['offshore'] > 0:
+            location = 'offshore'
+        basin_location_mapper[name] = location
+
+    return basin_location_mapper
+
+
+def construct_basin_location_mapper_with_external(df: pd.DataFrame, external_mapping: dict):
+    basin_location_info = df[['basin_name', 'onshore', 'offshore', 'onshore-offshore-calc']].to_dict("records")
+    basin_location_mapper = {}
+
+    for info in basin_location_info:
+        name = info['basin_name']
+
+        if name in external_mapping.keys():
+            location = external_mapping[name]
+        elif info['onshore-offshore-calc'] > 0:
+            location = 'onshore-offshore'
+        elif info['onshore'] > 0 and info['offshore'] == 0:
+            location = 'onshore'
+        elif info['onshore'] == 0 and info['offshore'] > 0:
+            location = 'offshore'
+        basin_location_mapper[name] = location
+
+    return basin_location_mapper
